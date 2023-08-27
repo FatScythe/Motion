139182e63ade8965a80bd449ca12234451fdcda8
Abdullahi Fahm Oladayo <holadhayo28@gmail.com>
2023-08-27 06:50:45 +0100

    Added: Sesions model

---
+++ src/models/session.model.ts
@@ -0,0 +1,22 @@
+import { Schema, model, Document, Types } from "mongoose";
+
+interface sessionDocument extends Document {
+  refreshToken: string;
+  ip: string;
+  valid: boolean;
+  userAgent: string;
+  user: Types.ObjectId;
+}
+
+const sessionSchema = new Schema<sessionDocument>(
+  {
+    refreshToken: { type: String, required: true },
+    ip: { type: String, required: true },
+    valid: { type: Boolean, default: true },
+    userAgent: { type: String, required: true },
+    user: { type: Schema.Types.ObjectId, ref: "Users", required: true },
+  },
+  { timestamps: true }
+);
+
+export default model<sessionDocument>("Sessions", sessionSchema);
