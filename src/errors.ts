33e07cc498a9a25047863521c6f0d6cdcbd8987f
Abdullahi Fahm Oladayo <holadhayo28@gmail.com>
2023-08-27 12:33:44 +0100

    Handling errors better

--- src/errors.ts
+++ src/errors.ts
@@ -1,33 +1,34 @@
 export class CustomAPIError extends Error {
-  constructor(public message: string) {
+  constructor(public message: string, public statusCode: number = 500) {
     super(message);
+    this.statusCode = statusCode;
   }
 }
 
 export class BadRequestError extends CustomAPIError {
-  constructor(public message: string, public code: number) {
+  constructor(public message: string, public statusCode: number = 400) {
     super(message);
-    this.code = 400;
+    this.statusCode = statusCode;
   }
 }
 
 export class NotFoundError extends CustomAPIError {
-  constructor(public message: string, public code: number) {
+  constructor(public message: string, public statusCode: number = 404) {
     super(message);
-    this.code = 404;
+    this.statusCode = statusCode;
   }
 }
 
 export class UnauthenticatedError extends CustomAPIError {
-  constructor(public message: string, public code: number) {
+  constructor(public message: string, public statusCode: number = 401) {
     super(message);
-    this.code = 401;
+    this.statusCode = statusCode;
   }
 }
 
 export class Unauthorized extends CustomAPIError {
-  constructor(public message: string, public code: number) {
+  constructor(public message: string, public statusCode: number = 403) {
     super(message);
-    this.code = 403;
+    this.statusCode = statusCode;
   }
 }
--- src/middlewares/error-handler.ts
+++ src/middlewares/error-handler.ts
@@ -1,14 +1,27 @@
 import { Request, Response, NextFunction } from "express";
 import chalk from "chalk";
+import { CustomAPIError } from "../errors";
+
 const errorHandleMiddleware = (
-  err: Error,
+  err: CustomAPIError,
   req: Request,
   res: Response,
   next: NextFunction
 ) => {
+  const customErr = {
+    msg:
+      err.message ||
+      "Something went wrong, our engineeers are currently working on it",
+    code: err.statusCode || 500,
+  };
   console.log(chalk.redBright("In error handler", err));
 
-  res.status(500).json({ msg: "Something went wrong" });
+  // @ts-ignore
+  if (err.name === "MongoServerError" && err.code === 11000) {
+    customErr.msg = "Duplicate fields";
+    customErr.code = 400;
+  }
+  res.status(customErr.code).json({ msg: customErr.msg });
 };
 
 export default errorHandleMiddleware;
