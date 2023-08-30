0968a80e7fd2fe9ed6df43b13bf1decb3c473689
Abdullahi Fahm Oladayo <holadhayo28@gmail.com>
2023-08-30 11:44:38 +0100

    Implemented varying rate limiter on the get single post route

--- src/router.ts
+++ src/router.ts
@@ -1,8 +1,10 @@
-import express from "express";
+import express, { NextFunction, Request, Response } from "express";
+import rateLimit from "express-rate-limit";
 import { registerUser, loginUser, logoutUser } from "./controllers/auth.ctrl";
 import { showMe } from "./controllers/user.ctrl";
 import {
   authenticateUser,
+  authenticateVisitor,
   authorizePermission,
 } from "./middlewares/authentication-handler";
 import {
@@ -13,7 +15,41 @@ import {
   deletePost,
 } from "./controllers/post.ctrl";
 
-const router = express();
+const router = express.Router();
+
+type limit = {
+  max: number;
+  message: string;
+};
+
+const isSubscribed = async (req: Request): Promise<limit> => {
+  if (req.user) {
+    if (req.user.role === "admin" || req.user.isSubscribed) {
+      // users that are  subscribed limited to 50req per hour
+      return { max: 200, message: "Hold on a moment" };
+    } else {
+      // users that are not subscribed limited to 5req per hour
+      return { max: 10, message: "Please subscibe to read more post" };
+    }
+  }
+
+  // Non - users limited to 3req per hour
+  return { max: 4, message: "Create an account to read more post" };
+};
+
+const getPostLimiter = rateLimit({
+  windowMs: 60 * 60 * 1000, // 1hr
+  standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+  legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+  max: async (request: Request, res: Response) => {
+    const value = await isSubscribed(request);
+    return value.max;
+  },
+  message: async (request: Request, res: Response) => {
+    const value = await isSubscribed(request);
+    return value.message;
+  },
+});
 
 router.post("/register", registerUser);
 
@@ -27,7 +63,7 @@ router.route("/post").get(getAllPost).post(authenticateUser, addPost);
 
 router
   .route("/post/:id")
-  .get(getSinglePost)
+  .get(authenticateVisitor, getPostLimiter, getSinglePost)
   .post(authenticateUser, addPost)
   .patch([authenticateUser, authorizePermission("admin", "editor")], editPost)
   .delete(
